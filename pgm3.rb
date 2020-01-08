11007947713fbf876c65842fe17371d6de9242ee
Sahla Muhammed Ali <41678780+Sahlamali@users.noreply.github.com>
2020-01-08 12:50:10 +0530

    Create pgm3.rb

---
+++ pgm3.rb
@@ -0,0 +1,21 @@
+=begin
+Program to illustrate boolean data type
+=end
+
+if true
+    puts "It is True"
+else
+    puts "It is False"
+end
+
+if nil
+    puts "It is nil"
+else
+    puts "It is not nil"
+end
+
+if 0
+    puts "0 is True"
+else 
+    puts "0 is False"
+end
